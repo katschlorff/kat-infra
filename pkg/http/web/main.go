de8c78e2fe9d529d1acc295f287b768793246df8
Kat Joy <kat.joy@proton.me>
2023-05-29 20:56:28 -0500

    Basic go file to serve the static stuff

---
+++ pkg/http/web/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+var prefixes = []string{"scrung", "bingus", "bong", "dangdiddly", "wumpscumbly"}
+var suffixes = []string{"gus", "go", "rnch", "scrimbus", "grimgis"}
+
+func kat_say() {
+	print(prefixes[rand.Intn(len(prefixes))] + suffixes[rand.Intn(len(suffixes))])
+}
+
+func main() {
+	// kat_say()
+	// Serve static files from the frontend/dist directory.
+	fs := http.FileServer(http.Dir("./app/dist"))
+	http.Handle("/", fs)
+
+	// Start the server.
+	fmt.Println("Server listening on port 8080")
+	log.Panic(
+		http.ListenAndServe(":8080", nil),
+	)
+}
